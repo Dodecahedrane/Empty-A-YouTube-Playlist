0f588b22b339229ce003fdca62710b934615bd01
Oliver Guy <oliver.guy-1@students.plymouth.ac.uk>
2021-10-17 22:51:16 +0100

    v1 semi tested

--- removeAllVideos.js
+++ removeAllVideos.js
@@ -1,1 +1,17 @@
-
+var deleted = 0;
+setInterval(function () {
+  deleted++;
+  if(deleted%64 === 0){
+    document.getElementById("spinner").scrollIntoView();
+  }
+  document.querySelector('#primary button[aria-label="Action menu"]').click();
+  var rmv = document.evaluate(
+    '//span[contains(text(),"Remove from")]',
+    document,
+    null,
+    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+  rmv.snapshotItem(0).click();
+  
+}, 100);
